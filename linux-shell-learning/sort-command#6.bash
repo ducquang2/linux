1beb0a6352609a8cea29ec240d6893b3ce922320
ducquang2 <quangbilun@gmail.com>
2020-10-30 21:05:33 +0700

    explain

--- linux-shell-learning/sort-command#6.bash
+++ linux-shell-learning/sort-command#6.bash
@@ -1,4 +1,4 @@
 sort -t$'\t' -k2 -n
 # -t$'\t' : tells that table column is separated by 'tab space'
-# -k : tells that sort the second table column
+# -k2 : tells that sort the second table column
 # -n : tells that denote column data is numeric
--- linux-shell-learning/sort-command#7.bash
+++ linux-shell-learning/sort-command#7.bash
@@ -1,1 +1,4 @@
 sort -t$"|" -k2 -rn
+# -t$'|' : tells that table column is separated by '|'
+# -k2 : tells that sort the second table column
+# -n : tells that denote column data is numeric
