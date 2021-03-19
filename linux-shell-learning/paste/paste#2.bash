3d15cf2948bc08444363376fab225e0d525d0664
ducquang2 <quangbilun@gmail.com>
2021-03-19 10:23:48 +0700

    update

---
+++ linux-shell-learning/paste/paste#2.bash
@@ -0,0 +1,7 @@
+paste -sd ';;\n'
+
+# paste -d';' - - -
+
+# - - - is for merging a file by pasting the data into 3 columns. In this case it is seperated by semicolon seperator.
+
+# As in our problem statement says to make three consecutive rows folded into one line. So - - - does this task for us.
